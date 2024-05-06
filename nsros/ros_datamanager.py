9a2e1dcd5c1f58c4a853a26149943d32728f02eb
Sivadinesh Ponrajan <sivadinesh107@gmail.com>
2024-05-06 13:23:01 +0200

    Modified the datamanager

--- nsros/ros_datamanager.py
+++ nsros/ros_datamanager.py
@@ -3,11 +3,17 @@ A datamanager for the NSROS Bridge.
 """
 
 from dataclasses import dataclass, field
-from typing import Type, Dict, Tuple
-
+from typing import Type, Dict, Tuple, Generic, cast, get_origin, get_args
+from typing_extensions import TypeVar
+from functools import cached_property
+from nerfstudio.utils.misc import get_orig_class
+import torch
 from rich.console import Console
 
-from nerfstudio.data.datamanagers import base_datamanager
+from nerfstudio.data.datamanagers.base_datamanager import (
+    VanillaDataManager,
+    VanillaDataManagerConfig
+)
 from nerfstudio.model_components.ray_generators import RayGenerator
 from nerfstudio.cameras.rays import RayBundle
 
@@ -20,7 +26,7 @@ CONSOLE = Console(width=120)
 
 
 @dataclass
-class ROSDataManagerConfig(base_datamanager.VanillaDataManagerConfig):
+class ROSDataManagerConfig(VanillaDataManagerConfig):
     """A ROS datamanager that handles a streaming dataloader."""
 
     _target: Type = field(default_factory=lambda: ROSDataManager)
@@ -34,9 +40,10 @@ class ROSDataManagerConfig(base_datamanager.VanillaDataManagerConfig):
     """ Number of images to train on (for dataset tensor pre-allocation). """
     topic_slop: float = 0.05
 
+TDataset = TypeVar("TDataset", bound=ROSDataset, default=ROSDataset)
 
 class ROSDataManager(
-    base_datamanager.VanillaDataManager
+    VanillaDataManager, Generic[TDataset]
 ):  # pylint: disable=abstract-method
     """Essentially the VannilaDataManager from Nerfstudio except that the
     typical dataloader for training images is replaced with one that streams
@@ -56,6 +63,20 @@ class ROSDataManager(
         return ROSDataset(
             dataparser_outputs=self.train_dataparser_outputs, device=self.device
         )
+    
+    @cached_property
+    def dataset_type(self) -> Type[TDataset]:
+        """
+        Returns the dataset type passed as the generic argument. 
+        
+        NOTE: Hacked from the Vanilla DataManager implementation.
+        """
+        default: Type[TDataset] = cast(TDataset, TDataset.__default__)  # type: ignore
+        orig_class: Type[ROSDataManager] = get_orig_class(self, default=None)  # type: ignore
+        if type(self) is ROSDataManager and orig_class is None:
+            return default
+        if orig_class is not None and get_origin(orig_class) is ROSDataManager:
+            return get_args(orig_class)[0]
 
     def setup_train(self):
         assert self.train_dataset is not None
